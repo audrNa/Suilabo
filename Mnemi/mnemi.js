2aa121f5bfa4f0a500ff3e0d4afd77b73c95e02f
audrNa <audrna7@gmail.com>
2024-02-04 13:41:21 +0800

    Replace Mnemi's paste button with clear button

--- Mnemi/mnemi.js
+++ Mnemi/mnemi.js
@@ -8,9 +8,10 @@ const MESSAGE_ERROR_UNKNWON = "それは誤用です。";
  * Bind select() to ACTION_SELECT.
  */
 document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case ACTION_SELECT: select()
-    }
+  switch (event.key) {
+    case ACTION_SELECT:
+      select();
+  }
 });
 
 /**
@@ -18,71 +19,68 @@ document.addEventListener("keydown", (event) => {
  * selected then it calls execute().
  */
 function select() {
-    var inputElement = document.getElementById("textInput");
-    if (document.activeElement === inputElement) {
-        execute();
-        inputElement.blur();
-    }
-    else {
-        inputElement.focus();
-    }
+  var inputElement = document.getElementById("textInput");
+  if (document.activeElement === inputElement) {
+    execute();
+    inputElement.blur();
+  } else {
+    inputElement.focus();
+  }
 }
 
 /**
  * Main function
  */
 function execute() {
-    // Add an extra newline so the very last part will always be sliced
-    var text = document.getElementById("textInput").value + '\n';
-    var coverLink = document.getElementById("coverInput").value;
+  // Add an extra newline so the very last part will always be sliced
+  var text = document.getElementById("textInput").value + "\n";
+  var coverLink = document.getElementById("coverInput").value;
 
-    // Handle empty text input
-    if (!text.trim()) {
-        setAlert(MESSAGE_EMPTY_TEXT, "danger");
-        return;
+  // Handle empty text input
+  if (!text.trim()) {
+    setAlert(MESSAGE_EMPTY_TEXT, "danger");
+    return;
+  }
+
+  // Handle empty cover link input
+  if (!coverLink.trim()) {
+    setAlert(MESSAGE_EMPTY_COVER_LINK, "danger");
+    return;
+  }
+
+  try {
+    // Slice text
+    var slices = [];
+    for (let checkpoint, i = 0; i < text.length; i++) {
+      if (text[i] == "\n") {
+        // Add 1 to checkpoint to ignore the '\n'
+        slices.push(text.slice(checkpoint + 1, i));
+        checkpoint = i;
+      }
     }
 
-    // Handle empty cover link input
-    if (!coverLink.trim()) {
-        setAlert(MESSAGE_EMPTY_COVER_LINK, "danger");
-        return;
+    // Get information from 0 - 2
+    let title = slices[0];
+    let titleOriginal = slices[1];
+    let code = slices[2].replace("#", "");
+
+    // Get all tags grouped by section
+    let sections = {};
+    if (isUserMobile()) {
+      sections = readSections(slices);
+    } else {
+      sections = groupSections(slices);
     }
 
-    try {
-        // Slice text
-        var slices = [];
-        for (let checkpoint, i = 0; i < text.length; i++) {
-            if (text[i] == "\n") {
-                // Add 1 to checkpoint to ignore the '\n'
-                slices.push(text.slice(checkpoint + 1, i))
-                checkpoint = i;
-            }
-        }
-
-        // Get information from 0 - 2
-        let title = slices[0];
-        let titleOriginal = slices[1];
-        let code = slices[2].replace('#', '');
-
-        // Get all tags grouped by section
-        let sections = {};
-        if (isUserMobile()) {
-            sections = readSections(slices);
-        } else {
-            sections = groupSections(slices);
-        }
-
-        // Create and set output
-        let output = createFormat(title, titleOriginal, code, coverLink, sections);
-        document.getElementById("output").innerHTML = output;
-        document.getElementById("embed").classList.replace("d-none", "d-block");
-    }
+    // Create and set output
+    let output = createFormat(title, titleOriginal, code, coverLink, sections);
+    document.getElementById("output").innerHTML = output;
+    document.getElementById("embed").classList.replace("d-none", "d-block");
+  } catch (error) {
     // Handle errors
-    catch (error) {
-        setAlert(MESSAGE_ERROR_UNKNWON, "danger");
-        console.log(error);
-    }
-
+    setAlert(MESSAGE_ERROR_UNKNWON, "danger");
+    console.log(error);
+  }
 }
 
 /**
@@ -91,28 +89,29 @@ function execute() {
  * @returns {Object}
  */
 function readSections(slices) {
-    let sections = {};
-    let currentSection;
-    let i = 3;
-    while (i < slices.length) {
-        // Get section
-        var index = slices[i].split(':');
-        currentSection = index[0];
-        // Use regex to split the tags
-        if (currentSection != "Pages") {
-            sections[currentSection] = index[1].split(/\d+K?/g);
-            sections[currentSection].pop();
-            sections[currentSection] = sections[currentSection].map(item => item.trim());
-
-        }
-        // Pages section is kinda special because all it contains is a number
-        else {
-            sections[currentSection] = [index[1].trim()];
-        }
-        // Next slice
-        i++;
+  let sections = {};
+  let currentSection;
+  let i = 3;
+  while (i < slices.length) {
+    // Get section
+    var index = slices[i].split(":");
+    currentSection = index[0];
+    // Use regex to split the tags
+    if (currentSection != "Pages") {
+      sections[currentSection] = index[1].split(/\d+K?/g);
+      sections[currentSection].pop();
+      sections[currentSection] = sections[currentSection].map((item) =>
+        item.trim()
+      );
     }
-    return sections;
+    // Pages section is kinda special because all it contains is a number
+    else {
+      sections[currentSection] = [index[1].trim()];
+    }
+    // Next slice
+    i++;
+  }
+  return sections;
 }
 
 /**
@@ -121,24 +120,24 @@ function readSections(slices) {
  * @returns {Object}
  */
 function groupSections(slices) {
-    let sections = {};
-    let currentSection;
-    let i = 3;
-    while (i < slices.length) {
-        let index = slices[i];
-        // Create a new array for this section
-        if (index.includes(':')) {
-            currentSection = index.trim().replace(':', '');
-            sections[currentSection] = [];
-        }
-        // Add tag to its section
-        else if (!isNumberK(index) || currentSection == "Pages") {
-            sections[currentSection].push(index);
-        }
-        // Next slice
-        i++;
+  let sections = {};
+  let currentSection;
+  let i = 3;
+  while (i < slices.length) {
+    let index = slices[i];
+    // Create a new array for this section
+    if (index.includes(":")) {
+      currentSection = index.trim().replace(":", "");
+      sections[currentSection] = [];
     }
-    return sections;
+    // Add tag to its section
+    else if (!isNumberK(index) || currentSection == "Pages") {
+      sections[currentSection].push(index);
+    }
+    // Next slice
+    i++;
+  }
+  return sections;
 }
 
 /**
@@ -151,31 +150,34 @@ function groupSections(slices) {
  * @returns {String}
  */
 function createFormat(title, titleOriginal, code, coverLink, sections) {
-    // Set initial information
-    let string =
-`**${title}**\n
+  // Set initial information
+  let string = `**${title}**\n
 ${titleOriginal}
-### [#${code}](${mahou('8h33oa3oaB3dcG3283fuGfuGrrb8h3Gdsrrb3oaHiKG4hDh2rrbGds3oafuG55hfuG') + code})
+### [#${code}](${
+    mahou(
+      "8h33oa3oaB3dcG3283fuGfuGrrb8h3Gdsrrb3oaHiKG4hDh2rrbGds3oafuG55hfuG"
+    ) + code
+  })
 `;
 
-    // Set all tags and sections
-    for (let section of Object.keys(sections)) {
-        string += `> **${section}:** ${sections[section].join(", ")}\n`
-    }
+  // Set all tags and sections
+  for (let section of Object.keys(sections)) {
+    string += `> **${section}:** ${sections[section].join(", ")}\n`;
+  }
 
-    string += `[。](${coverLink})`;
+  string += `[。](${coverLink})`;
 
-    return string;
+  return string;
 }
 
 /**
  * Closes any alert on the page.
  */
 function closeAlert() {
-    let alertCloseButton = document.getElementById("alertClose");
-    if (alertCloseButton) {
-        alertCloseButton.click();
-    }
+  let alertCloseButton = document.getElementById("alertClose");
+  if (alertCloseButton) {
+    alertCloseButton.click();
+  }
 }
 
 /**
@@ -184,8 +186,8 @@ function closeAlert() {
  * @param {String} type Bootstrap type of the alert.
  */
 function setAlert(message, type) {
-    const alertPlaceholder = document.getElementById('messageOutput');
-    alertPlaceholder.innerHTML = `
+  const alertPlaceholder = document.getElementById("messageOutput");
+  alertPlaceholder.innerHTML = `
     <div class="alert alert-${type} alert-dismissible fade show text-start" role="alert">
         <div>${message}</div>
         <button id="alertClose" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -198,27 +200,28 @@ function setAlert(message, type) {
  * @returns {String}
  */
 function mahou(s) {
-    let genryou = "sfhkJd3dBFHSDf4d2I1fC28hdhEhBv3bSg2og4hDh2fuG7HpbVfa217270dbcBdf3yp2wpVpHfA283bf2Fg3zx8Hr3ppPfm3N3bnub2vfsegdHHhhhgg301212hIiIbQgUjiywyoWwjheWrgBhFbFFKIunnEPQWqcolkIjdhga3gGfhcoclolLMaOXdIKIhUHiKufOkgGggTGdsGh555h8h3G4h4UbE0NN211kRrrby5tB3dcd99eBcG33oaaAxzf37g99Wn93bHn37plp3ErymsLI";
-    let arufa = {};
-    for (let c = 0, i = 3; i <= genryou.length; i += 3) {
-        let index = i / 3 + 32;
-        arufa[String.fromCharCode(index)] = genryou.slice(c, i);
-        c = i;
-    }
+  let genryou =
+    "sfhkJd3dBFHSDf4d2I1fC28hdhEhBv3bSg2og4hDh2fuG7HpbVfa217270dbcBdf3yp2wpVpHfA283bf2Fg3zx8Hr3ppPfm3N3bnub2vfsegdHHhhhgg301212hIiIbQgUjiywyoWwjheWrgBhFbFFKIunnEPQWqcolkIjdhga3gGfhcoclolLMaOXdIKIhUHiKufOkgGggTGdsGh555h8h3G4h4UbE0NN211kRrrby5tB3dcd99eBcG33oaaAxzf37g99Wn93bHn37plp3ErymsLI";
+  let arufa = {};
+  for (let c = 0, i = 3; i <= genryou.length; i += 3) {
+    let index = i / 3 + 32;
+    arufa[String.fromCharCode(index)] = genryou.slice(c, i);
+    c = i;
+  }
 
-    let angou = {};
-    for (let a of Object.keys(arufa)) {
-        angou[arufa[a]] = a;
-    }
+  let angou = {};
+  for (let a of Object.keys(arufa)) {
+    angou[arufa[a]] = a;
+  }
 
-    let dec = "";
-    for (let c = 0, i = 3; i <= s.length; i += 3) {
-        let chunk = s.slice(c, i);
-        dec += angou[chunk];
-        c = i;
-    }
+  let dec = "";
+  for (let c = 0, i = 3; i <= s.length; i += 3) {
+    let chunk = s.slice(c, i);
+    dec += angou[chunk];
+    c = i;
+  }
 
-    return dec;
+  return dec;
 }
 
 /**
@@ -226,21 +229,28 @@ function mahou(s) {
  * @param {String} text
  */
 function copyToClipboard(text) {
-    navigator.clipboard.writeText(text);
+  navigator.clipboard.writeText(text);
 }
 
 /**
  * Automatically paste from the user's clipboard into an input element's value.
- * @param {Node} element
+ * @param {String} elementId
  */
-async function pasteFromClipboard(element) {
-    try {
-        let result = await navigator.clipboard.readText();
-        document.getElementById(element).value = result;
-    }
-    catch (error) {
-        console.log("Error: Failed to read clipboard.");
-    }
+async function pasteFromClipboard(elementId) {
+  try {
+    let result = await navigator.clipboard.readText();
+    document.getElementById(elementId).value = result;
+  } catch (error) {
+    console.log("Error: Failed to read clipboard.");
+  }
+}
+
+/**
+ * Clears input value from an input element
+ * @param {String} elementId
+ */
+function clearInput(elementId) {
+  document.getElementById(elementId).value = "";
 }
 
 /**
@@ -249,12 +259,12 @@ async function pasteFromClipboard(element) {
  * @returns {Boolean}
  */
 function isNumberK(x) {
-    if (!x) {
-        return false;
-    }
+  if (!x) {
+    return false;
+  }
 
-    x = Number(x.toUpperCase().replace('K', ''));
-    return !isNaN(x);
+  x = Number(x.toUpperCase().replace("K", ""));
+  return !isNaN(x);
 }
 
 /**
@@ -263,11 +273,11 @@ function isNumberK(x) {
  * @returns {Boolean}
  */
 function isNumber(x) {
-    if (!x) {
-        return false;
-    }
+  if (!x) {
+    return false;
+  }
 
-    return !isNaN(Number(x));
+  return !isNaN(Number(x));
 }
 
 /**
@@ -275,15 +285,17 @@ function isNumber(x) {
  * @returns {Boolean}
  */
 function isUserMobile() {
-    if (navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)) {
-       return true;
-    } else {
-       return false;
-    }
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
